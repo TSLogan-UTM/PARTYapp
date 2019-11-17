e40fb9cda1d82660a3ccb9655c91e853c5a024bb
Vincent Manoli <vinomano@ut.utm.edu>
2019-11-16 18:07:41 -0600

    First GUI has underwent possible temporary changes and a second window containing the canvas is underway.

--- PARTYapp/MainWindow.xaml.cs
+++ PARTYapp/MainWindow.xaml.cs
@@ -15,15 +15,62 @@ using System.Windows.Shapes;
 
 namespace PARTYapp
 {
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        
+        private void CreateButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            PARTY_WINDOW app = new PARTY_WINDOW();
+
+            app.Show();
+            this.Close();
+            app.HB.Text = HeightBox.Text;
+            app.WB.Text = WidthBox.Text;
+        }
+
+        private void HeightSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            HeightSlider.Value = Math.Round(HeightSlider.Value, 0);
+            HeightBox.Text = Convert.ToString(HeightSlider.Value);
+        }
+
+        private void HeightBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            HeightSlider.IsSnapToTickEnabled = true;
+            HeightSlider.TickPlacement = System.Windows.Controls.Primitives.TickPlacement.TopLeft;
+            HeightSlider.TickFrequency = 25;
+            HeightBox.IsReadOnly = true;
+            HeightBox.Text = Convert.ToString(HeightSlider.Value);
+            
+        }
+
+        private void WidthSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            WidthSlider.Value = Math.Round(WidthSlider.Value, 0);
+            WidthBox.Text = Convert.ToString(WidthSlider.Value);
+        }
+
+
+        private void WidthBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            WidthSlider.IsSnapToTickEnabled = true;
+            WidthSlider.TickPlacement = System.Windows.Controls.Primitives.TickPlacement.TopLeft;
+            WidthSlider.TickFrequency = 25;
+            WidthBox.IsReadOnly = true;
+            WidthBox.Text = Convert.ToString(WidthSlider.Value);
+        }
+
         public MainWindow()
         {
             
             InitializeComponent();
+
+            
         }
     }
 }
---
+++ PARTYapp/PARTY_WINDOW.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Brushes = System.Windows.Media.Brushes;
+using Point = System.Windows.Point;
+
+
+namespace PARTYapp
+{
+    
+    /// <summary>
+    /// Interaction logic for PARTY_WINDOW.xaml
+    /// </summary>
+    public partial class PARTY_WINDOW : Window
+    {
+        public PARTY_WINDOW()
+        {
+
+            
+
+            InitializeComponent();
+
+            GRID_100.ShowGridLines = false;
+            GRID_75.ShowGridLines = false;
+            //GRID_50.ShowGridLines = false;
+            GRID_25.ShowGridLines = false;
+            int count = 1;
+
+
+            int width = 50;
+            int height = 50;
+
+            for (int i = 0; i < width - 1; i++)
+            {
+                for (int j = 0; j < height - 1; j++)
+                {
+                    Button MyControl1 = new Button();
+                    MyControl1.Content = "";
+                    MyControl1.Name = "Button" + count.ToString();
+                    MyControl1.Background = Brushes.White;
+                    if(count%2 > 0)
+                    {
+                        MyControl1.Background = Brushes.Gray;
+                    }
+
+                    Grid.SetColumn(MyControl1, j);
+                    Grid.SetRow(MyControl1, i);
+                    GRID_50.Children.Add(MyControl1);
+
+                    count++;
+                }
+
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void WB_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            WB.IsReadOnly = true;
+        }
+
+        private void HB_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            HB.IsReadOnly = true;
+
+            
+        }
+
+        private void CtrlAltYeet_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Are you Sure?", "PARTY FOUL", MessageBoxButton.YesNo);
+
+            if(result == MessageBoxResult.Yes)
+            {
+                App.Current.Shutdown();
+            }
+            else
+            {
+                return;
+            }
+
+            
+        }
+
+
+    }
+}
