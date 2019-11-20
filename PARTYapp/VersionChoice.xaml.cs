6b90151c81fe1d0f3743c599529093588a67e81c
Thomas <thosloga@ut.utm.edu>
2019-11-20 00:17:33 -0600

    Have a version selector up and running. Need to confirm this will count as a factory pattern. Several changes. See Dev Notes.

---
+++ PARTYapp/VersionChoice.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PARTYapp
+{
+    /// <summary>
+    /// Interaction logic for VersionChoice.xaml
+    /// </summary>
+    public partial class VersionChoice : Window
+    {
+        public VersionChoice()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow basic = new MainWindow();
+            this.Close();
+            basic.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Advanced advanced = new Advanced();
+            this.Close();
+            advanced.Show();
+        }
+    }
+}
